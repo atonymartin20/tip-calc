635d43ba6ad03e0a158913c0c762797138c94f44
Alex Martin <atonymartin20@yahoo.com>
2021-09-09 14:24:05 -0500

    Starter Code added

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		<logo />
+		<router-view />
+	</div>
+</template>
+
+<script>
+	import Logo from './components/logo/logo.vue';
+
+	export default {
+		name: 'App', 
+		components: { Logo }
+	}
+</script>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap');
+
+	#app {
+		font-family: 'Space Mono', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%; 
+		height: 1024px;
+		min-height: 100vh;
+		flex-direction: column;
+		background: #C5E4E7;
+	}
+</style>
