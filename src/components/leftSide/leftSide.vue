8cee49bc514f7c76e532de5281b8e3b7e2f2304e
Alex Martin <atonymartin20@yahoo.com>
2021-09-09 17:54:25 -0500

    Create leftSide.vue

---
+++ src/components/leftSide/leftSide.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class='left-side'>
+        <bill />
+        <selectTip />
+        <numberOfPeople />
+    </div>
+</template>
+
+<script>
+    import Bill from '../bill/bill.vue';
+    import SelectTip from '../selectTip/selectTip.vue';
+    import NumberOfPeople from '../numberOfPeople/numberOfPeople.vue';
+
+	export default {
+		name: 'leftSide',
+        components: { Bill, SelectTip, NumberOfPeople }
+	}
+</script>
+
+<style scoped>
+    .left-side {
+        width: 100%;
+        max-width: 379px;
+        height: 388px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+</style>
