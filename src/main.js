74087a96291b173c53e88ac339cba48fccee8850
Alex Martin <atonymartin20@yahoo.com>
2021-09-16 15:22:22 -0500

    Added store into main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from './store'
+import { createStore } from './store'
 
 Vue.config.productionTip = false
+
+const store = createStore()
 
 /* eslint-disable no-new */
 new Vue({
