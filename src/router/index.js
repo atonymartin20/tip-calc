6da2b20f4fdbdb3aa653d0cbc9cb714bc661ea69
Alex Martin <atonymartin20@yahoo.com>
2021-09-18 21:40:02 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import MainContainer from '@/components/MainContainer/MainContainer.vue'
+import MainContainer from '../components/mainContainer/mainContainer.vue'
 
 Vue.use(Router)
 
